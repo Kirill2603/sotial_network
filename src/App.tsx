bad1373dfe06839c7259167b765f5b8180adaaa0
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-30 20:43:19 +0300

    sotial network
    add antd layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  MessageOutlined,
+  SettingOutlined,
+  GlobalOutlined,
+  CustomerServiceOutlined,
+  ProfileOutlined,
+} from '@ant-design/icons';
+
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+
+
 function App() {
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout  style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={()=> setCollapsed(!collapsed)}>
+          <div className="logo" />
+          <Menu theme='dark' defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<ProfileOutlined />}>
+              Profile
+            </Menu.Item>
+            <Menu.Item key="2" icon={<MessageOutlined />}>
+              Messages
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<GlobalOutlined />} title="News">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<CustomerServiceOutlined />} title="Music">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<SettingOutlined />}>
+              Settings
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
