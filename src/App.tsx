210eeb2fb4940707a3f67050708fce2f4d4dde9d
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-31 01:28:48 +0300

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React,{useState} from 'react';
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Layout, Menu, Avatar, Space} from 'antd';
 import {
   MessageOutlined,
   SettingOutlined,
   GlobalOutlined,
   CustomerServiceOutlined,
   ProfileOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
 
 import './App.css';
+import Search from "antd/es/input/Search";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
-
 
 
 function App() {
@@ -22,23 +23,23 @@ function App() {
   return (
     <div className="App">
       <Layout  style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={()=> setCollapsed(!collapsed)}>
+        <Sider theme='dark' collapsible collapsed={collapsed} onCollapse={()=> setCollapsed(!collapsed)}>
           <div className="logo" />
           <Menu theme='dark' defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1" icon={<ProfileOutlined />}>
               Profile
             </Menu.Item>
-            <Menu.Item key="2" icon={<MessageOutlined />}>
-              Messages
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<GlobalOutlined />} title="News">
+            <SubMenu key="sub1" icon={< MessageOutlined/>} title="Messages">
               <Menu.Item key="3">Tom</Menu.Item>
               <Menu.Item key="4">Bill</Menu.Item>
               <Menu.Item key="5">Alex</Menu.Item>
             </SubMenu>
+            <Menu.Item key="2" icon={<GlobalOutlined />}>
+              News
+            </Menu.Item>
             <SubMenu key="sub2" icon={<CustomerServiceOutlined />} title="Music">
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
+              <Menu.Item key="6">Playlists</Menu.Item>
+              <Menu.Item key="8">Charts</Menu.Item>
             </SubMenu>
             <Menu.Item key="9" icon={<SettingOutlined />}>
               Settings
@@ -46,12 +47,17 @@ function App() {
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+
+
+
+            <Space align="center">
+              center
+              <Search placeholder="input search text" allowClear style={{ width: 200 }} />
+              <Avatar shape="square" icon={<UserOutlined />} />
+            </Space>
+          </Header>
+          <Content style={{ margin: '16px 16px' }}>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
               Bill is a cat.
             </div>
